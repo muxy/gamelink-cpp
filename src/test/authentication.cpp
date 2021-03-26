d7fba79a36e02ec750ec530c5aca0d5a701ea2a5
Cameron Currie <me@cameroncurrie.net>
2021-03-25 19:28:43 -0500

    Add AuthenticateWithJWT to SDK

--- src/test/authentication.cpp
+++ src/test/authentication.cpp
@@ -3,7 +3,7 @@
 
 #include "../gamelink.hpp"
 
-TEST_CASE("SDK Authentication", "[sdk][authentication]")
+TEST_CASE("SDK PIN Authentication", "[sdk][authentication][pin]")
 {
 	gamelink::SDK sdk;
 	std::string jwt = "test-jwt";
@@ -20,8 +20,45 @@ TEST_CASE("SDK Authentication", "[sdk][authentication]")
 
 	sdk.ForeachSend([](gamelink::Send* send) {
 		REQUIRE(JSONEquals(send->data,
-			R"({"action":"authenticate","data":{"client_id":"client_id","pin":"pin"},"params":{"request_id":65535}})"
-		));
+						   R"({"action":"authenticate","data":{"client_id":"client_id","pin":"pin"},"params":{"request_id":65535}})"));
+	});
+
+	REQUIRE(!sdk.HasSends());
+
+	// Verify state after successful auth
+	sdk.ReceiveMessage(R"({
+		"meta": {
+			"action": "authenticate"
+		},
+
+		"data": {
+			"jwt": "test-jwt"
+		}
+	})");
+
+	REQUIRE(sdk.IsAuthenticated());
+	REQUIRE(sdk.GetUser()->GetJWT() == jwt);
+	REQUIRE(!sdk.HasSends());
+}
+
+TEST_CASE("SDK JWT Authentication", "[sdk][authentication][jwt]")
+{
+	gamelink::SDK sdk;
+	std::string jwt = "test-jwt";
+
+	// Test initial state
+	REQUIRE(!sdk.IsAuthenticated());
+	REQUIRE(sdk.GetUser() == NULL);
+	REQUIRE(!sdk.HasSends());
+
+	// Verify generated auth request
+	sdk.AuthenticateWithJWT("client_id", jwt);
+
+	REQUIRE(sdk.HasSends());
+
+	sdk.ForeachSend([](gamelink::Send* send) {
+		REQUIRE(JSONEquals(send->data,
+						   R"({"action":"authenticate","data":{"client_id":"client_id","jwt":"test-jwt"},"params":{"request_id":65535}})"));
 	});
 
 	REQUIRE(!sdk.HasSends());
