0a6f556e7ca4d7b88dbeb3b245e225e581138e9b
albertywang <albertywang@gmail.com>
2021-04-13 15:11:04 -0500

    Fix amalgam generation so that common usage cases are supported.

--- amalgam_check.cpp
+++ amalgam_check.cpp
@@ -1,4 +1,8 @@
 
+// Simulate including a header with gamelink_single.hpp before 
+// trying to define the implementation.
+#include "gamelink_single.hpp"
+
 #define MUXY_GAMELINK_SINGLE_IMPL
 #include "gamelink_single.hpp"
 
