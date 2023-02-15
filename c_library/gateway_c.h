03e9ee59946cef0fcff5e3c7f908409fe1e355ec
Troy <tshanks@muxy.io>
2023-02-15 14:35:08 -0500

    gateway function fixes and dll exporting

--- c_library/gateway_c.h
+++ c_library/gateway_c.h
@@ -3,151 +3,184 @@
 #include <stdint.h>
 #include <stddef.h>
 
-typedef uint16_t MGW_RequestID;
-static const MGW_RequestID MGW_ANY_REQUEST_ID      = UINT16_MAX;
-static const MGW_RequestID MGW_REJECTED_REQUEST_ID = UINT16_MAX-1;
-
-typedef struct
+#ifdef __cplusplus
+extern "C"
 {
-    void *SDK;
-} MGW_SDK;
+#endif
+	/*
+		!!!!!! WARNING !!!!!!
+		Any function that returns a MGW_String must be freed with MGW_FreeString or you will have a memory leak!
+	*/
 
-typedef struct
-{
-    const char *JWT;
-    const char *RefreshToken;
-    const char *TwitchName;
-    bool HasError;
-} MGW_AuthenticateResponse;
+#ifdef MUXY_CLIB_EXPORT
+#ifdef _MSC_VER
+#define MUXY_GW_CLIB_API __declspec(dllexport)
+#else
+#define MUXY_GW_CLIB_API
+#endif
+#else
+#define MUXY_GW_CLIB_API
+#endif
 
-typedef struct
-{
-    const char *GameName;
-    // Base64 encoded image
-    const char *GameLogo;
-    const char *Theme;
-} MGW_GameMetadata;
+	typedef char* MGW_String;
+	typedef uint16_t MGW_RequestID;
+	static const MGW_RequestID MGW_ANY_REQUEST_ID = UINT16_MAX;
+	static const MGW_RequestID MGW_REJECTED_REQUEST_ID = UINT16_MAX - 1;
 
-typedef struct
-{
-    const char *Bytes;
-    size_t Length;
-} MGW_Payload;
+	typedef struct
+	{
+		void* SDK;
+	} MGW_SDK;
 
-typedef void (*MGW_AuthenticateResponseCallback)(void *User, const MGW_AuthenticateResponse *AuthResp);
-typedef void (*MGW_PayloadCallback)(void *User, const MGW_Payload *Payload);
+	typedef struct
+	{
+		const char* JWT;
+		const char* RefreshToken;
+		const char* TwitchName;
+		bool HasError;
+	} MGW_AuthenticateResponse;
 
-MGW_SDK  MGW_MakeSDK(const char* GameID);
-void         MGW_KillSDK(MGW_SDK Gateway);
-MGW_RequestID MGW_SDK_AuthenticateWithPIN(MGW_SDK Gateway, const char *PIN, MGW_AuthenticateResponseCallback Callback, void *User);
-MGW_RequestID MGW_SDK_AuthenticateWithRefreshToken(MGW_SDK Gateway, const char *RefreshToken, MGW_AuthenticateResponseCallback Callback, void *User);
-bool         MGW_SDK_IsAuthenticated(MGW_SDK Gateway);
+	typedef struct
+	{
+		const char* GameName;
+		// Base64 encoded image
+		const char* GameLogo;
+		const char* Theme;
+	} MGW_GameMetadata;
 
-MGW_RequestID MGW_SDK_SetGameMetadata(MGW_SDK Gateway, const MGW_GameMetadata *Meta);
+	typedef struct
+	{
+		const char* Bytes;
+		size_t Length;
+	} MGW_Payload;
 
-bool MGW_SDK_ReceiveMessage(MGW_SDK Gateway, const char *Bytes, uint32_t Length);
-bool MGW_SDK_HasPayloads(MGW_SDK Gateway);
-void MGW_SDK_ForeachPayload(MGW_SDK Gateway, MGW_PayloadCallback Callback, void *User);
+	typedef void (*MGW_AuthenticateResponseCallback)(void* User, const MGW_AuthenticateResponse* AuthResp);
+	typedef void (*MGW_PayloadCallback)(void* User, const MGW_Payload* Payload);
 
-const char* MGW_SDK_GetSandboxURL(MGW_SDK Gateway);
-const char* MGW_SDK_GetProductionURL(MGW_SDK Gateway);
+	MUXY_GW_CLIB_API MGW_SDK MGW_MakeSDK(const char* GameID);
+	MUXY_GW_CLIB_API void MGW_KillSDK(MGW_SDK Gateway);
+	MUXY_GW_CLIB_API MGW_RequestID MGW_SDK_AuthenticateWithPIN(MGW_SDK Gateway, const char* PIN, MGW_AuthenticateResponseCallback Callback, void* User);
+	MUXY_GW_CLIB_API MGW_RequestID MGW_SDK_AuthenticateWithRefreshToken(MGW_SDK Gateway, const char* RefreshToken, MGW_AuthenticateResponseCallback Callback, void* User);
+	MUXY_GW_CLIB_API bool MGW_SDK_IsAuthenticated(MGW_SDK Gateway);
 
-// Polling.
-static const int MGW_POLL_LOCATION_DEFAULT = 0;
-static const int MGW_POLL_MODE_CHAOS = 0;
-static const int MGW_POLL_MODE_ORDER = 1;
+	MUXY_GW_CLIB_API MGW_RequestID MGW_SDK_SetGameMetadata(MGW_SDK Gateway, const MGW_GameMetadata* Meta);
 
-typedef struct {
-	int Winner;
-	int WinningVoteCount;
+	MUXY_GW_CLIB_API bool MGW_SDK_ReceiveMessage(MGW_SDK Gateway, const char* Bytes, uint32_t Length);
+	MUXY_GW_CLIB_API bool MGW_SDK_HasPayloads(MGW_SDK Gateway);
+	MUXY_GW_CLIB_API void MGW_SDK_ForeachPayload(MGW_SDK Gateway, MGW_PayloadCallback Callback, void* User);
 
-	const int* Results;
-	size_t ResultCount;
+	MUXY_GW_CLIB_API MGW_String MGW_SDK_GetSandboxURL(MGW_SDK Gateway);
+	MUXY_GW_CLIB_API MGW_String MGW_SDK_GetProductionURL(MGW_SDK Gateway);
 
-	int Count;
-	double Mean;
-	bool IsFinal;
-} MGW_PollUpdate;
-
-typedef void (*MGW_PollUpdateCallback)(void*, MGW_PollUpdate*);
-
-typedef struct {
-	const char* Prompt;
-
-	int Location;
-	int Mode;
-
-	const char** Options;
-	size_t OptionsCount;
-
-	int Duration;
-
-	MGW_PollUpdateCallback OnUpdate;
-	void* User;
-} MGW_PollConfiguration;
-
-void MGW_SDK_StartPoll(MGW_SDK Gateway, MGW_PollConfiguration config);
-void MGW_SDK_StopPoll(MGW_SDK Gateway);
+	MUXY_GW_CLIB_API void MGW_FreeString(MGW_String Str);
 
 
-static const int MGW_ACTIONSTATE_UNAVAILABLE = 0;
-static const int MGW_ACTIONSTATE_AVAILABLE = 1;
-static const int MGW_ACTIONSTATE_HIDDEN = 2;
+	// Polling.
+	static const int MGW_POLL_LOCATION_DEFAULT = 0;
+	static const int MGW_POLL_MODE_CHAOS = 0;
+	static const int MGW_POLL_MODE_ORDER = 1;
 
-static const int MGW_ACTIONCATEGORY_NEUTRAL = 0;
-static const int MGW_ACTIONCATEGORY_HINDER = 1;
-static const int MGW_ACTIONCATEGORY_HELP = 2;
+	typedef struct
+	{
+		int Winner;
+		int WinningVoteCount;
 
-static const int MGW_ACTION_INFINITE_USES = -1;
+		const int* Results;
+		size_t ResultCount;
 
-typedef struct {
-	const char* ID;
-	int Category;
-	int State;
-	int Impact;
+		int Count;
+		double Mean;
+		bool IsFinal;
+	} MGW_PollUpdate;
 
-	const char* Name;
-	const char* Description;
-	const char* Icon;
+	typedef void (*MGW_PollUpdateCallback)(void*, MGW_PollUpdate*);
 
-	int Count;
-} MGW_Action;
+	typedef struct
+	{
+		const char* Prompt;
 
-void MGW_SDK_SetActions(MGW_SDK Gateway, const MGW_Action* Begin, const MGW_Action* End);
-void MGW_SDK_EnableAction(MGW_SDK Gateway, const char* id);
-void MGW_SDK_DisableAction(MGW_SDK Gateway, const char* id);
-void MGW_SDK_SetActionCount(MGW_SDK Gateway, const char* id, int count);
+		int Location;
+		int Mode;
 
-typedef struct {
-	const char* Label;
-	const char* Value;
-	const char* Icon;
-} MGW_GameText;
+		const char** Options;
+		size_t OptionsCount;
 
-typedef struct {
-	MGW_GameText* Texts;
-	size_t TextsCount;
-} MGW_GameTexts;
+		int Duration;
 
-void MGW_SDK_SetGameTexts(MGW_SDK Gateway, const MGW_GameTexts* Texts);
+		MGW_PollUpdateCallback OnUpdate;
+		void* User;
+	} MGW_PollConfiguration;
 
-typedef struct {
-	const char* TransactionID;
-	const char* SKU;
-	int Bits;
-} MGW_BitsUsed;
+	void MGW_SDK_StartPoll(MGW_SDK Gateway, MGW_PollConfiguration config);
+	void MGW_SDK_StopPoll(MGW_SDK Gateway);
 
-typedef struct {
-	const char* TransactionID;
-	const char* SKU;
-	int Cost;
-} MGW_ActionUsed;
+	static const int MGW_ACTIONSTATE_UNAVAILABLE = 0;
+	static const int MGW_ACTIONSTATE_AVAILABLE = 1;
+	static const int MGW_ACTIONSTATE_HIDDEN = 2;
 
-typedef void (*MGW_OnBitsUsedCallback)(void*, MGW_BitsUsed*);
-typedef void (*MGW_OnActionUsedCallback)(void*, MGW_ActionUsed*);
+	static const int MGW_ACTIONCATEGORY_NEUTRAL = 0;
+	static const int MGW_ACTIONCATEGORY_HINDER = 1;
+	static const int MGW_ACTIONCATEGORY_HELP = 2;
 
-void MGW_SDK_OnBitsUsed(MGW_SDK Gateway, MGW_OnBitsUsedCallback Callback, void* User);
-void MGW_SDK_OnActionUsed(MGW_SDK Gateway, MGW_OnActionUsedCallback Callback, void* User);
+	static const int MGW_ACTION_INFINITE_USES = -1;
 
-void MGW_SDK_ValidateActionTransaction(MGW_SDK Gateway, MGW_ActionUsed Coins, const char* Reason);
-void MGW_SDK_RefundActionTransaction(MGW_SDK Gateway, MGW_ActionUsed Coins, const char* Reason);
+	typedef struct
+	{
+		const char* ID;
+		int Category;
+		int State;
+		int Impact;
+
+		const char* Name;
+		const char* Description;
+		const char* Icon;
+
+		int Count;
+	} MGW_Action;
+
+	MUXY_GW_CLIB_API void MGW_SDK_SetActions(MGW_SDK Gateway, const MGW_Action* Begin, const MGW_Action* End);
+	MUXY_GW_CLIB_API void MGW_SDK_EnableAction(MGW_SDK Gateway, const char* id);
+	MUXY_GW_CLIB_API void MGW_SDK_DisableAction(MGW_SDK Gateway, const char* id);
+	MUXY_GW_CLIB_API void MGW_SDK_SetActionCount(MGW_SDK Gateway, const char* id, int count);
+
+	typedef struct
+	{
+		const char* Label;
+		const char* Value;
+		const char* Icon;
+	} MGW_GameText;
+
+	typedef struct
+	{
+		MGW_GameText* Texts;
+		size_t TextsCount;
+	} MGW_GameTexts;
+
+	void MGW_SDK_SetGameTexts(MGW_SDK Gateway, const MGW_GameTexts* Texts);
+
+	typedef struct
+	{
+		const char* TransactionID;
+		const char* SKU;
+		int Bits;
+	} MGW_BitsUsed;
+
+	typedef struct
+	{
+		const char* TransactionID;
+		const char* SKU;
+		int Cost;
+	} MGW_ActionUsed;
+
+	typedef void (*MGW_OnBitsUsedCallback)(void*, MGW_BitsUsed*);
+	typedef void (*MGW_OnActionUsedCallback)(void*, MGW_ActionUsed*);
+
+	MUXY_GW_CLIB_API void MGW_SDK_OnBitsUsed(MGW_SDK Gateway, MGW_OnBitsUsedCallback Callback, void* User);
+	MUXY_GW_CLIB_API void MGW_SDK_OnActionUsed(MGW_SDK Gateway, MGW_OnActionUsedCallback Callback, void* User);
+
+	void MGW_SDK_ValidateActionTransaction(MGW_SDK Gateway, MGW_ActionUsed Coins, const char* Reason);
+	void MGW_SDK_RefundActionTransaction(MGW_SDK Gateway, MGW_ActionUsed Coins, const char* Reason);
+
+#ifdef __cplusplus
+}
+#endif
