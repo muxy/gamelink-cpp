c9cda850a85463247f413f2d114658c51cad402f
Albert <albertywang@gmail.com>
2023-02-16 14:30:56 -0600

    Fixing some whitespace errors

--- c_library/gateway_c.cpp
+++ c_library/gateway_c.cpp
@@ -7,23 +7,23 @@
 MGW_SDK MGW_MakeSDK(const char* GameID)
 {
     MGW_SDK Gateway;
-	Gateway.SDK = new gateway::SDK(gateway::string(GameID));
+    Gateway.SDK = new gateway::SDK(gateway::string(GameID));
 
     return Gateway;
 }
 
 void MGW_KillSDK(MGW_SDK Gateway)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
-	delete SDK;
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    delete SDK;
 }
 
 MGW_RequestID MGW_SDK_AuthenticateWithPIN(MGW_SDK Gateway, const char *PIN, MGW_AuthenticateResponseCallback Callback, void *User)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
     return SDK->AuthenticateWithPIN(PIN, [=](const gateway::AuthenticateResponse Resp)
     {
-		MGW_AuthenticateResponse Auth;
+        MGW_AuthenticateResponse Auth;
         Auth.JWT          = Resp.JWT.c_str();
         Auth.RefreshToken = Resp.RefreshToken.c_str();
         Auth.TwitchName   = Resp.TwitchName.c_str();
@@ -35,10 +35,10 @@ MGW_RequestID MGW_SDK_AuthenticateWithPIN(MGW_SDK Gateway, const char *PIN, MGW_
 MGW_RequestID MGW_SDK_AuthenticateWithRefreshToken(MGW_SDK Gateway, const char *RefreshToken, MGW_AuthenticateResponseCallback Callback, void *User)
 {
     
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
     return SDK->AuthenticateWithRefreshToken(RefreshToken, [=](const gateway::AuthenticateResponse Resp)
     {
-		MGW_AuthenticateResponse Auth;
+        MGW_AuthenticateResponse Auth;
         Auth.JWT          = Resp.JWT.c_str();
         Auth.RefreshToken = Resp.RefreshToken.c_str();
         Auth.TwitchName   = Resp.TwitchName.c_str();
@@ -49,19 +49,19 @@ MGW_RequestID MGW_SDK_AuthenticateWithRefreshToken(MGW_SDK Gateway, const char *
 
 bool MGW_SDK_IsAuthenticated(MGW_SDK Gateway)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
     return SDK->IsAuthenticated();
 }
 
 void MGW_SDK_HandleReconnect(MGW_SDK Gateway)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
-	SDK->HandleReconnect();
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    SDK->HandleReconnect();
 }
 
 MGW_RequestID MGW_SDK_SetGameMetadata(MGW_SDK Gateway, MGW_GameMetadata Meta)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
     gateway::GameMetadata MD;
     MD.GameName = Meta.GameName;
     MD.GameLogo = Meta.GameLogo;
@@ -71,13 +71,13 @@ MGW_RequestID MGW_SDK_SetGameMetadata(MGW_SDK Gateway, MGW_GameMetadata Meta)
 
 bool MGW_SDK_ReceiveMessage(MGW_SDK Gateway, const char *Bytes, uint32_t Length)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
     return SDK->ReceiveMessage(Bytes, Length);
 }
 
 bool MGW_SDK_HasPayloads(MGW_SDK Gateway)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
     return SDK->HasPayloads();
 }
 
@@ -89,7 +89,7 @@ typedef struct
 
 static void ForeachPayloadCallback(void *User, const gateway::Payload *Payload)
 {
-	MGW_ForeachPayloadUserdata* data = static_cast<MGW_ForeachPayloadUserdata*>(User);
+    MGW_ForeachPayloadUserdata* data = static_cast<MGW_ForeachPayloadUserdata*>(User);
 
     MGW_Payload PL;
     PL.Bytes = Payload->GetData();
@@ -100,190 +100,190 @@ static void ForeachPayloadCallback(void *User, const gateway::Payload *Payload)
 
 void MGW_SDK_ForeachPayload(MGW_SDK Gateway, MGW_PayloadCallback Callback, void *User)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
 
-	MGW_ForeachPayloadUserdata data;
-	data.Callback = Callback;
-	data.User = User;
+    MGW_ForeachPayloadUserdata data;
+    data.Callback = Callback;
+    data.User = User;
 
     SDK->ForeachPayload(ForeachPayloadCallback, &data);
 }
 
 MGW_String MGW_SDK_GetSandboxURL(MGW_SDK Gateway)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
-	return MuxyGameLink_StrDup(SDK->GetSandboxURL().c_str());
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    return MuxyGameLink_StrDup(SDK->GetSandboxURL().c_str());
 }
 
 MGW_String MGW_SDK_GetProductionURL(MGW_SDK Gateway)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
-	return MuxyGameLink_StrDup(SDK->GetProductionURL().c_str());
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    return MuxyGameLink_StrDup(SDK->GetProductionURL().c_str());
 }
 
 void MGW_SDK_StartPoll(MGW_SDK Gateway, MGW_PollConfiguration cfg)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
 
-	gateway::PollConfiguration config;
-	config.Duration = cfg.Duration;
-	config.Location = static_cast<gateway::PollLocation>(cfg.Location);
-	config.Mode = static_cast<gateway::PollMode>(cfg.Mode);
-	config.Prompt = gamelink::string(cfg.Prompt);
-	
-	for (size_t i = 0; i < cfg.OptionsCount; ++i)
-	{
-		config.Options.push_back(gamelink::string(cfg.Options[i]));
-	}
+    gateway::PollConfiguration config;
+    config.Duration = cfg.Duration;
+    config.Location = static_cast<gateway::PollLocation>(cfg.Location);
+    config.Mode = static_cast<gateway::PollMode>(cfg.Mode);
+    config.Prompt = gamelink::string(cfg.Prompt);
+    
+    for (size_t i = 0; i < cfg.OptionsCount; ++i)
+    {
+        config.Options.push_back(gamelink::string(cfg.Options[i]));
+    }
 
-	config.OnUpdate = [=](const gateway::PollUpdate& update)
-	{
-		MGW_PollUpdate upd;
-		upd.Winner = update.Winner;
-		upd.WinningVoteCount = update.WinningVoteCount;
-		upd.ResultCount = update.Results.size();
-		
-		upd.Results = nullptr;
-		if (!update.Results.empty())
-		{
-			upd.Results = update.Results.data();
-		}
+    config.OnUpdate = [=](const gateway::PollUpdate& update)
+    {
+        MGW_PollUpdate upd;
+        upd.Winner = update.Winner;
+        upd.WinningVoteCount = update.WinningVoteCount;
+        upd.ResultCount = update.Results.size();
+        
+        upd.Results = nullptr;
+        if (!update.Results.empty())
+        {
+            upd.Results = update.Results.data();
+        }
 
-		upd.Count = update.Count;
-		upd.Mean = update.Mean;
-		upd.IsFinal = update.IsFinal;
+        upd.Count = update.Count;
+        upd.Mean = update.Mean;
+        upd.IsFinal = update.IsFinal;
 
-		cfg.OnUpdate(cfg.User, &upd);
-	};
+        cfg.OnUpdate(cfg.User, &upd);
+    };
 
-	SDK->StartPoll(config);
+    SDK->StartPoll(config);
 }
 
 void MGW_SDK_StopPoll(MGW_SDK Gateway)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
-	SDK->StopPoll();
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    SDK->StopPoll();
 }
 
 void MGW_SDK_SetActions(MGW_SDK Gateway, const MGW_Action* Begin, const MGW_Action* End)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
 
-	std::vector<gateway::Action> actions;
-	while (Begin != End)
-	{
-		gateway::Action action;
-		action.ID = gamelink::string(Begin->ID);
-		action.Category = static_cast<gateway::ActionCategory>(Begin->Category);
-		action.State = static_cast<gateway::ActionState>(Begin->State);
-		action.Impact = Begin->Impact;
-		action.Name = gamelink::string(Begin->Name);
-		action.Description = gamelink::string(Begin->Description);
-		action.Icon = gamelink::string(Begin->Icon);
-		action.Count = Begin->Count;
+    std::vector<gateway::Action> actions;
+    while (Begin != End)
+    {
+        gateway::Action action;
+        action.ID = gamelink::string(Begin->ID);
+        action.Category = static_cast<gateway::ActionCategory>(Begin->Category);
+        action.State = static_cast<gateway::ActionState>(Begin->State);
+        action.Impact = Begin->Impact;
+        action.Name = gamelink::string(Begin->Name);
+        action.Description = gamelink::string(Begin->Description);
+        action.Icon = gamelink::string(Begin->Icon);
+        action.Count = Begin->Count;
 
-		actions.emplace_back(std::move(action));
+        actions.emplace_back(std::move(action));
 
-		Begin++;
-	}
+        Begin++;
+    }
 
-	if (!actions.empty())
-	{
-		SDK->SetActions(actions.data(), actions.data() + actions.size());
-	}
+    if (!actions.empty())
+    {
+        SDK->SetActions(actions.data(), actions.data() + actions.size());
+    }
 }
 
 void MGW_SDK_EnableAction(MGW_SDK Gateway, const char* id)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
-	SDK->EnableAction(gamelink::string(id));
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    SDK->EnableAction(gamelink::string(id));
 }
 
 void MGW_SDK_DisableAction(MGW_SDK Gateway, const char* id)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
-	SDK->DisableAction(gamelink::string(id));
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    SDK->DisableAction(gamelink::string(id));
 }
 
 void MGW_SDK_SetActionCount(MGW_SDK Gateway, const char* id, int count)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
-	SDK->SetActionCount(gamelink::string(id), count);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    SDK->SetActionCount(gamelink::string(id), count);
 }
 
 void MGW_SDK_SetGameTexts(MGW_SDK Gateway, const MGW_GameTexts* Texts)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
 
-	gateway::GameTexts texts;
-	for (size_t i = 0; i < Texts->TextsCount; ++i)
-	{
-		gateway::GameText entry;
-		entry.Label = gateway::string(Texts->Texts[i].Label);
-		entry.Value = gateway::string(Texts->Texts[i].Value);
-		entry.Icon = gateway::string(Texts->Texts[i].Icon);
+    gateway::GameTexts texts;
+    for (size_t i = 0; i < Texts->TextsCount; ++i)
+    {
+        gateway::GameText entry;
+        entry.Label = gateway::string(Texts->Texts[i].Label);
+        entry.Value = gateway::string(Texts->Texts[i].Value);
+        entry.Icon = gateway::string(Texts->Texts[i].Icon);
 
-		texts.Texts.emplace_back(std::move(entry));
-	}
+        texts.Texts.emplace_back(std::move(entry));
+    }
 
-	SDK->SetGameTexts(texts);
+    SDK->SetGameTexts(texts);
 }
 
 void MGW_SDK_OnBitsUsed(MGW_SDK Gateway, MGW_OnBitsUsedCallback Callback, void* User)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
 
-	SDK->OnBitsUsed([=](const gateway::BitsUsed& BitsUsed)
-	{
-		MGW_BitsUsed Used;
-		Used.TransactionID = BitsUsed.TransactionID.c_str();
-		Used.SKU = BitsUsed.SKU.c_str();
-		Used.Bits = BitsUsed.Bits;
+    SDK->OnBitsUsed([=](const gateway::BitsUsed& BitsUsed)
+    {
+        MGW_BitsUsed Used;
+        Used.TransactionID = BitsUsed.TransactionID.c_str();
+        Used.SKU = BitsUsed.SKU.c_str();
+        Used.Bits = BitsUsed.Bits;
 
-		Callback(User, &Used);
-	});
+        Callback(User, &Used);
+    });
 }
 
 void MGW_SDK_OnActionUsed(MGW_SDK Gateway, MGW_OnActionUsedCallback Callback, void* User)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
 
-	SDK->OnActionUsed([=](const gateway::ActionUsed& BitsUsed)
-	{
-		MGW_ActionUsed Used;
-		Used.TransactionID = BitsUsed.TransactionID.c_str();
-		Used.SKU = BitsUsed.SKU.c_str();
-		Used.Cost = BitsUsed.Cost;
+    SDK->OnActionUsed([=](const gateway::ActionUsed& BitsUsed)
+    {
+        MGW_ActionUsed Used;
+        Used.TransactionID = BitsUsed.TransactionID.c_str();
+        Used.SKU = BitsUsed.SKU.c_str();
+        Used.Cost = BitsUsed.Cost;
 
-		Callback(User, &Used);
-	});
+        Callback(User, &Used);
+    });
 }
 
 void MGW_SDK_ValidateTransaction(MGW_SDK Gateway, MGW_ActionUsed Coins, const char* Reason)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
 
-	gateway::ActionUsed tx;
-	tx.SKU = gateway::string(Coins.SKU);
-	tx.TransactionID = gateway::string(Coins.TransactionID);
-	tx.Cost = Coins.Cost;
+    gateway::ActionUsed tx;
+    tx.SKU = gateway::string(Coins.SKU);
+    tx.TransactionID = gateway::string(Coins.TransactionID);
+    tx.Cost = Coins.Cost;
 
-	SDK->ValidateActionTransaction(tx, gateway::string(Reason));
+    SDK->ValidateActionTransaction(tx, gateway::string(Reason));
 }
 
 void MGW_SDK_RefundTransaction(MGW_SDK Gateway, MGW_ActionUsed Coins, const char* Reason)
 {
-	gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
+    gateway::SDK* SDK = static_cast<gateway::SDK*>(Gateway.SDK);
 
-	gateway::ActionUsed tx;
-	tx.SKU = gateway::string(Coins.SKU);
-	tx.TransactionID = gateway::string(Coins.TransactionID);
-	tx.Cost = Coins.Cost;
+    gateway::ActionUsed tx;
+    tx.SKU = gateway::string(Coins.SKU);
+    tx.TransactionID = gateway::string(Coins.TransactionID);
+    tx.Cost = Coins.Cost;
 
-	SDK->RefundActionTransaction(tx, gateway::string(Reason));
+    SDK->RefundActionTransaction(tx, gateway::string(Reason));
 }
 
 void MGW_FreeString(MGW_String Str)
 {
-	free(Str);
+    free(Str);
 }
